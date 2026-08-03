3c935d4dd1c5cdf266e5bd7746989903aa30a0e1
Fatma Sena Arpinar <sena@s172-20-164-h245.paws.uga.edu>
2026-08-03 17:21:43 -0400

    add canonical retrieval, publication display

--- src/components/DynamicMetadataForm.js
+++ src/components/DynamicMetadataForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
 import {
   Box,
   Chip,
@@ -17,14 +17,35 @@ import MultiselectTextInput from "./MultiSelectTextInput";
 import { Button, Col, Row } from "react-bootstrap";
 import ComplexFieldTable from "./ComplexFieldTable";
 import MultiTextInput from "./MultiTextInput";
+import TextAlert from "./TextAlert";
+import DialogAlert from "../components/DialogAlert";
+import { Loading } from "./Loading";
+import { axiosError } from "../utils/axiosError";
+import { getJson } from "../utils/api";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function DynamicMetadataForm({
   fields,
   values,
   onChange,
 }) {
-  const error = () => {
-    console.log("error!");
+
+  const [showLoading, setShowLoading] = useState(false);
+  const [alertDialogInput, setAlertDialogInput] = useReducer(
+          (state, newState) => ({ ...state, ...newState }),
+          { show: false, id: "" }
+  );
+
+  const [textAlertInput, setTextAlertInput] = useReducer(
+      (state, newState) => ({ ...state, ...newState }),
+      { show: false, id: "" }
+  );
+
+  const [selectedPublication, setSelectedPublication] = useState(null);
+  const [publicationCache, setPublicationCache] = useState({});
+
+  const error = (errorMessage) => {
+    setTextAlertInput(errorMessage);
   }
   
   const updateValue = (fieldId, value) => {
@@ -54,6 +75,31 @@ export default function DynamicMetadataForm({
     );
   };
 
+  const getPublication = (pubId, event) => {
+      if (publicationCache[pubId]) {
+          setSelectedPublication (publicationCache[pubId]);
+      }
+      else {
+          setShowLoading(true);
+          // get the publication details
+          getJson ("api/util/getpublication?identifier=" + pubId).then (({ data }) => {
+              if (data.data) {
+                  setSelectedPublication(data.data);
+                  publicationCache[pubId] = data.data;
+                  setShowLoading(false);
+              }
+          }).catch(function(error) {
+              if (error && error.response && error.response.data) {
+                  setTextAlertInput ({"show": true, "message": error.response.data.message });
+                  setShowLoading(false);
+                  return;
+              } else {
+                  axiosError(error, null, setAlertDialogInput);
+              }
+          });
+      }
+  }
+
   const renderField = (field) => {
     const label = (
       <>
@@ -71,9 +117,6 @@ export default function DynamicMetadataForm({
       </>
     );
 
-    //
-    // MULTI VALUE AUTOCOMPLETE
-    //
     if (field.type === "autocomplete") {
       return (
         <FormControl fullWidth variant="outlined">
@@ -133,7 +176,6 @@ export default function DynamicMetadataForm({
       </>);
     }
 
-    // MULTI TEXT
     if (field.type === "text" && field.multiple) {
       return (
         <>
@@ -166,6 +208,76 @@ export default function DynamicMetadataForm({
         </Col>
         </Row>
         </>
+      );
+    }
+
+    if (field.type === "publication") {
+      return (
+      <FormControl fullWidth variant="outlined">
+        <Row>
+        <Col xs={2} sm={3}> {label} </Col>
+        <Col xs={10} sm={9}>
+        <TextField
+          size="small"
+          fullWidth
+          value={values[field.id] || ""}
+          onChange={(e) => {
+            if (e.target.value === "") setSelectedPublication(null);
+            updateValue(field.id, e.target.value);
+          }}
+          onBlur={(e) => {
+              if (e.target.value) {
+                getPublication(e.target.value);
+              }
+            }}
+        />
+        </Col></Row>
+        {selectedPublication && 
+        <Box>
+          <Typography sx={{ p: 2 }}>
+                              <div>
+                              <h6 style={{ marginBottom: "3px" }}>
+                              <strong>{selectedPublication.title}</strong>
+                              </h6>
+                          </div>
+          
+                          <div style={{ textAlign: "left", paddingLeft: "35px" }}>
+                              <div>{selectedPublication.authors}</div>
+                              <div>
+                              {selectedPublication.journal} <span>&nbsp;</span>({selectedPublication.year})
+                              </div>
+                              <div>
+                              <FontAwesomeIcon icon={["fas", "book-open"]} size="sm" title="Book" />
+          
+                              {selectedPublication.pubmedId && 
+                              <>
+                              <span style={{ paddingLeft: "15px" }}>PMID:&nbsp;</span>
+                              <a
+                                  href={`https://pubmed.ncbi.nlm.nih.gov/${selectedPublication.pubmedId}`}
+                                  target="_blank"
+                                  rel="noopener noreferrer"
+                              >
+                                  {selectedPublication.pubmedId}
+                              </a>
+                              </>
+                              }
+                              {selectedPublication.doiId && 
+                              <>
+                              <span style={{ paddingLeft: "15px" }}>DOI:&nbsp;</span>
+                              <a
+                                  href={`https://doi.org/${selectedPublication.doiId}`}
+                                  target="_blank"
+                                  rel="noopener noreferrer"
+                              >
+                                  {selectedPublication.doiId}
+                              </a>
+                              </>
+                              }
+                              </div>
+                          </div>
+                          </Typography>
+        </Box>}
+      </FormControl>
       );
     }
 
@@ -209,6 +321,9 @@ export default function DynamicMetadataForm({
   };
 
   return (
+    <>
+    <TextAlert alertInput={textAlertInput}/>
+    <Loading show={showLoading}></Loading>
     <Grid 
         container
 				style={{ margin: "0 0 0 -12px" }}
@@ -220,5 +335,6 @@ export default function DynamicMetadataForm({
         </Grid>
       ))}
     </Grid>
+    </>
   );
 }
--- src/components/MultiAutoComplete.js
+++ src/components/MultiAutoComplete.js
@@ -1,7 +1,8 @@
-import { Autocomplete, Chip, TextField } from "@mui/material";
+import { Autocomplete, Chip, Dialog, DialogActions, DialogContent, DialogTitle, FormControl, FormControlLabel, Radio, RadioGroup, TextField, Typography } from "@mui/material";
 import React, { useRef, useState } from "react";
 import PropTypes from "prop-types";
-import { getAuthorizationHeader, getJson } from "../utils/api";
+import { getAuthorizationHeader, getJson, postJson } from "../utils/api";
+import { Button } from "react-bootstrap";
 
 export default function MultiAutoComplete (props) {
     const [options, setOptions] = useState([]);
@@ -9,6 +10,10 @@ export default function MultiAutoComplete (props) {
     const inputValueRef = useRef (props.inputValue);
     inputValueRef.current = props.inputValue;
 
+    const [enableMultiValueSelect, setEnableMultiValueSelect] = useState(false);
+    const [selectedCanonical, setSelectedCanonical] = useState(null);
+    const [canonicalForm, setCanonicalForm] = useState([]);
+    const [selectedValue, setSelectedValue] = useState(null);
 
     /**
 	 * Function to handle change event for input text.
@@ -16,11 +21,152 @@ export default function MultiAutoComplete (props) {
 	 * @param {string} value input value.
 	 * @param {string} reason event reason.
 	 **/
-	const handleChange = (event, value, reason) => {
+	const handleChange = async (event, value, reason) => {
 		if (!(event === null && value === "" && reason === "reset")){
 			props.setInputValue(props.fieldId, value);
-		}
+		} 
+        if (reason === "selectOption") {
+            if (props.multiple) {
+                if (reason === "removeOption") {
+                    props.setInputValue(props.fieldId, value);
+                    return;
+                }
+
+                const newestValue = value[value.length - 1];
+
+                const canonical = await getCanonicalForm(
+                    props.namespace,
+                    newestValue
+                );
+
+                if (!canonical) {
+                    return;
+                }
+
+                const updated = [
+                    ...value.slice(0, value.length - 1),
+                    canonical
+                ];
+
+                props.setInputValue(
+                    props.fieldId,
+                    updated
+                );
+
+                return;
+            }
+
+            const canonical = await getCanonicalForm(
+                props.namespace,
+                value
+            );
+
+            if (canonical) {
+                props.setInputValue(
+                    props.fieldId,
+                    canonical
+                );
+            }
+        }
 	};
+
+    const onInputChange = (event, value, reason) => {
+        setSearchInput(value);
+    }
+
+    function multiValueDialog () {
+        return (
+            <Dialog
+                    maxWidth="sm"
+                    fullWidth="true"
+                    aria-labelledby="multivalue-modal-title"
+                    aria-describedby="multivaluee-modal-description"
+                    scroll="paper"
+                    
+                    sx={{ //You can copy the code below in your theme
+                        '& .MuiBackdrop-root': {
+                          backgroundColor: 'transparent' // Try to remove this to see the result
+                        }
+                      }}
+                    open={enableMultiValueSelect}
+                    onClose={(event, reason) => {
+                        if (reason && reason === "backdropClick")
+                            return;
+                        setEnableMultiValueSelect(false);
+                        setSelectedCanonical(null);
+                    }}
+                >
+                    <DialogTitle id="multivalue-modal-title">
+                        <Typography id="multivalue-modal-title" variant="h6" component="h2">
+                        There are multiple matches for this selection. Select one: 
+                        </Typography>
+                    </DialogTitle>
+                    <DialogContent dividers>
+                        {canonicalForm && 
+                        <FormControl>
+                        <RadioGroup
+                          aria-labelledby="demo-radio-buttons-group-label"
+                          name="radio-buttons-group"
+                          defaultValue={canonicalForm[0] && canonicalForm[0].label}
+                        >
+                        {canonicalForm.map ((val, i) => {
+                            return <FormControlLabel 
+                                value={val.label} 
+                                control={<Radio />} 
+                                label={val.label}
+                                onChange={(event) => {
+                                    setSelectedCanonical (event.target.value);
+                                }} />
+                        })}
+                        </RadioGroup>
+                      </FormControl>}
+                    </DialogContent>
+                    <DialogActions>
+                        <Button className="gg-btn-blue-reg"
+                            onClick={()=> {
+                                const canonical = selectedCanonical || canonicalForm[0].label;
+                                if (props.multiple) {
+                                    const current = [...props.inputValue];
+                                    current[current.length - 1] = canonical;
+                                    props.setInputValue(props.fieldId, current);
+                                } else {
+                                    props.setInputValue(props.fieldId, canonical);
+                                }
+                                setEnableMultiValueSelect (false);
+                                setSelectedCanonical(null);
+                            }}>Select</Button>
+                    </DialogActions>
+            </Dialog>
+        )
+    }
+
+
+    async function getCanonicalForm(namespace, value) {
+        const response = await postJson(
+            "api/util/getcanonicalform?namespace=" +
+                namespace +
+                "&value=" +
+                encodeURIComponent(value),
+            null,
+            getAuthorizationHeader()
+        );
+
+        const matches = response.data?.data;
+
+        if (!matches || matches.length === 0) {
+            return value;
+        }
+
+        if (matches.length === 1) {
+            return matches[0].label;
+        }
+
+        // open dialog if multiple
+        setCanonicalForm(matches);
+        setEnableMultiValueSelect(true);
+
+        return null;
+    }
 
     const getTypeAhead =  (namespace, searchTerm) => {
         return getJson ("api/util/gettypeahead?namespace=" + namespace + "&limit=10&value=" + encodeURIComponent(searchTerm), 
@@ -44,22 +190,20 @@ export default function MultiAutoComplete (props) {
 	}, [searchInput, props.inputValue, props.namespace]);
 
 return (
+    <>
+    {enableMultiValueSelect && multiValueDialog()} 
     <Autocomplete
         freeSolo
         disabled={props.disabled}
         multiple={props.multiple}
         value={props.inputValue}
-        onInputChange={(event, newInputValue) => {
-            setSearchInput(newInputValue);
-        }}
+        onInputChange={onInputChange}
         onClose={(event, reason) => {
             console.log("closing reason " + reason );
-            /*if (options[index].length === 0) return;
             if ((reason === "selectOption" || reason === "blur")) {
                 getCanonicalForm (props.namespace, 
-                    reason === "selectOption" ? event.target.textContent : event.target.value, 
-                    index);
-            }*/
+                    reason === "selectOption" ? event.target.textContent : event.target.value);
+            }
         }}
         isOptionEqualToValue={(option, value) => (option === value)}
         options={options}
@@ -84,11 +228,15 @@ return (
                 variant='outlined'
                 placeholder={props.placeholder} />
         )}
-    />);
+    />
+    </>);
 }
 
 MultiAutoComplete.propTypes = {
-  inputValue: PropTypes.string,
+  inputValue: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.array
+  ]),
   placeholder: PropTypes.string,
   namespace: PropTypes.string,
   disabled: PropTypes.bool,
